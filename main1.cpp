061ec77766c3962a09ff32c5ee15038b80f1d7c1
kimsubin <chocobingsu81@naver.com>
2022-05-02 09:39:10 +0900

    5월 2일 과제

---
+++ main1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 2;
+	cout << a + 5 << endl;
+	cout << a - 5 << endl;
+	cout << a * 5 << endl;
+	cout << a / 5 << endl;
+	cout << a % 5 << endl;
+
+	float b = 3.4f;
+	cout << b + 7 << endl;
+	cout << b - 7 << endl;
+	cout << b * 7 << endl;
+	cout << b / 7 << endl;
+
+		return 0;
+}
